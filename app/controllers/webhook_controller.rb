605a54dc1d140982bf6b1785f21cbf894e8100ce
keisuke <rememberthatk@icloud.com>
2016-09-05 22:46:01 +0900

    as

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -15,7 +15,6 @@ class WebhookController < ApplicationController
     text_message = result['content']['text']
     from_mid =result['content']['from']
 
-    binding.pry
     client = LineClient.new(CHANNEL_ID, CHANNEL_SECRET, CHANNEL_MID, OUTBOUND_PROXY)
     res = client.send([from_mid], text_message)
 
